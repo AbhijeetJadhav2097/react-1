fd1eb4b7295c223b0fd8376373126e6ddff3a66c
Abhijeet Jadhav <abhijeetjadhav2097@gmail.com>
2023-07-13 13:35:55 +0530

    react-1 topic props completed

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Parent from './component/parent/index';
+import Child from './component/child/index';
+function App() {
+  return (
+    <div className="App">
+      <Parent />
+      <Child />
+      
+    </div>
+  );
+}
+
+export default App;
